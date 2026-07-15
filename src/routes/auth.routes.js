a1047370cc3db4070e6983bc0b36af67711c41ae
Rohit Ranjan Yadav <iamshareefg@gmail.com>
2026-07-15 14:31:15 +0545

    feat(auth): implement logout functionality and update routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,15 +1,23 @@
 import { Router } from "express";
 import { healthCheck } from "../controllers/healthcheck.controllers.js";
-import { login, registerUser } from "../controllers/auth.controllers.js";
+import {
+  login,
+  logoutUser,
+  registerUser,
+} from "../controllers/auth.controllers.js";
 import {
   userLoginValidator,
   userRegisterValidator,
 } from "../validators/index.js";
 import { validate } from "../middlewares/validator.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(userRegisterValidator(), validate, registerUser);
 router.route("/login").post(userLoginValidator(), validate, login);
+
+// Protected routes
+router.route("/logout").post(verifyJWT, logoutUser);
 
 export default router;
