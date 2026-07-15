d03b5d7a30bfc9f7afec5dc32d4b31aea0e9ba5f
Rohit Ranjan Yadav <iamshareefg@gmail.com>
2026-07-15 12:23:07 +0545

    feat(auth): implement user login functionality with token generation

--- notes.md
+++ notes.md
@@ -5,3 +5,12 @@
 4. Save the new user(Access Token, Refresh Token, General Token, sendmail)
 5. user verification => email
 6. send response to the user
+
+
+# Login a user
+1. take data from the user
+2. validate
+3. check if user exists
+4. check if password is correct
+5. generate tokens
+6. send tokens in cookies (cookie-parser)
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { healthCheck } from "../controllers/healthcheck.controllers.js";
-import { registerUser } from "../controllers/auth.controllers.js";
+import { login, registerUser } from "../controllers/auth.controllers.js";
 
 const router = Router();
 
 router.route("/register").post(registerUser);
+router.route("/login").post(login);
 
 export default router;
