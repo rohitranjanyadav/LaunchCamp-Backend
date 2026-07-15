8a869518c80607a1bf8ee711b43e4580f4d0ef64
Rohit Ranjan Yadav <iamshareefg@gmail.com>
2026-07-15 12:39:09 +0545

    feat(auth): add user registration validation and middleware

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { healthCheck } from "../controllers/healthcheck.controllers.js";
 import { login, registerUser } from "../controllers/auth.controllers.js";
+import { userRegisterValidator } from "../validators/index.js";
+import { validate } from "../middlewares/validator.middleware.js";
 
 const router = Router();
 
-router.route("/register").post(registerUser);
+router.route("/register").post(userRegisterValidator(), validate, registerUser);
 router.route("/login").post(login);
 
 export default router;
